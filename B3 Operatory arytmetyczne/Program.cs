b5b77e0b17fd9eea282858240e4b4aab6b91c40a
Kitsune <kitsune.dev@gmail.com>
2021-10-05 23:02:05 +0200

    B3 Operatory arytmetyczne

---
+++ B3 Operatory arytmetyczne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace B3_Operatory_arytmetyczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 7;
+
+            Console.WriteLine( a + " + " + b + " = " + (a + b) );
+            Console.WriteLine( a + " - " + b + " = " + (a - b) );
+            Console.WriteLine( a + " * " + b + " = " + (a * b) );
+            Console.WriteLine( a + " / " + b + " = " + (a / b) );
+            
+            // Modulo operator. This operator is keeping remainder from divide.
+            Console.WriteLine( a + " % " + b + " = " + (a % b) );
+
+            Console.ReadLine();
+        }
+    }
+}
