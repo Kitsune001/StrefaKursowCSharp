a516ef24b3bf469a3cb6140d35af4fade8e3a770
Kitsune <kitsune.dev@gmail.com>
2021-10-05 03:32:38 +0200

    A2 Komentarze

---
+++ A2 Komentarze/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A2_Komentarze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            // Console.WriteLine("Ten  tekst się nie wyświetli");
+            
+            // Kometarz jedoliniowy
+            
+            /*
+             * Komentarz
+             * wieloliniowy
+             */
+            
+            Console.WriteLine("Ten teks się wyświetli");
+            Console.ReadLine();
+        }
+    }
+}
