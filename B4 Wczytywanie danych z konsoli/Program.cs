cbfbedeb56e3f6df92e996d5aea525a4be3fcf3b
Kitsune <kitsune.dev@gmail.com>
2021-10-06 00:03:00 +0200

    B4 Wczytywanie danych z konsoli

---
+++ B4 Wczytywanie danych z konsoli/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace B4_Wczytywanie_danych_z_konsoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            
+            Console.WriteLine("Stranger: Ouch, I didn't knew someone is here. What's your name?");
+            Console.Write("You: My name is: ");
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Stranger: Hello, " + name + ", nice to meet you.");
+
+            Console.Read();
+        }
+    }
+}
