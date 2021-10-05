37d6ffbaaf23b53998440dd46ab1355e9f68a86f
Kitsune <kitsune.dev@gmail.com>
2021-10-05 03:44:53 +0200

    B1 Zmienne

---
+++ B1 Zmienne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace B1_Zmienne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklaracja i inicjalizacja
+            var a = 11;
+            
+            a = 24; // Zmiana wartości zmiennej a
+            
+            Console.WriteLine(a);   // Zostanie wyświetlone 24
+            
+            a = 35; // Zmiana wartości zmiennej a
+            
+            Console.ReadLine();
+        }
+    }
+}
