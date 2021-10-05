aab5d25810025a7589871980d2bc82202515136d
Kitsune <kitsune.dev@gmail.com>
2021-10-05 03:24:08 +0200

    A1 Pierwszy program

---
+++ A1 Pierwszy program/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace A1_Pierwszy_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To jest pierwsza linia tekstu");
+            Console.WriteLine("Druga linia tekstu");
+            Console.ReadLine();
+        }
+    }
+}
