9d5a3f9697704aa7bfc824a33cbae29642a588e8
Kitsune <kitsune.dev@gmail.com>
2021-10-07 21:57:14 +0200

    D2 Instrukcja switch

---
+++ D2 Instrukcja switch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace D2_Instrukcja_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("X = 1");
+                    break;
+                case 2:
+                    Console.WriteLine("X = 2");
+                    break;
+                case 3:
+                    Console.WriteLine("X = 3");
+                    break;
+                default:
+                    Console.WriteLine("X is not equal 1, 2 or 3.");
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
+}
