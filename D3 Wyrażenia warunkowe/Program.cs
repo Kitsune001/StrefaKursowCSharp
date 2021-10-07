a2466e02f55d2e8eeb8d604d70987d81f3aa1012
Kitsune <kitsune.dev@gmail.com>
2021-10-07 22:41:11 +0200

    D3 Wyrażenia warunkowe

---
+++ D3 Wyrażenia warunkowe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace D3_Wyrażenia_warunkowe
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var playerHealth = 0;
+            
+            Console.Write("Player HP: ");
+            playerHealth = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(playerHealth > 0 ? "Player is alive." : "Player is dead.");
+
+            Console.Read();
+        }
+    }
+}
