0242f6353765a95bc1d592114dce0915e77358b5
Kitsune <kitsune.dev@gmail.com>
2021-10-08 23:59:49 +0200

    D4 Operatory relacyjne

---
+++ D4 Operatory relacyjne/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace D4_Operatory_relacyjne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 5;
+            
+            Console.WriteLine("X: " + x);
+            Console.WriteLine("Y: " + y);
+            
+            Console.Write("X == Y : ");
+            Console.WriteLine(x == y);
+            
+            Console.Write("X != Y : ");
+            Console.WriteLine(x != y);
+
+            Console.Write("X > Y : ");
+            Console.WriteLine(x > y);
+            
+            Console.Write("X < Y : ");
+            Console.WriteLine(x < y);
+            
+            Console.Read();
+        }
+    }
+}
