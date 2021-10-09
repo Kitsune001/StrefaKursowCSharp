26e9951e5ada6454ba29fa33f9b0b595d9863116
Kitsune <kitsune.dev@gmail.com>
2021-10-09 05:47:39 +0200

    E1 Petla for

---
+++ E1 Petla for/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace E1_Petla_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000); // Sleep app for 1 sec.
+            }
+            Console.WriteLine("Boom");
+
+            Console.Read();
+        }
+    }
+}
