0743de368e655c550d0d8c29509758a2bcb019db
Kitsune <kitsune.dev@gmail.com>
2021-10-09 07:02:50 +0200

    E3 Petle while i do while

---
+++ E3 Petle while i do while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace E3_Petle_while_i_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 20)
+            {
+                Console.WriteLine("Hello nr: " + i);
+                i++;
+            }
+            
+            int j = 1;
+            do
+            {
+                Console.WriteLine("Is 1 bigger than 3? I don't think so...");
+            } while (j > 3);
+
+            Console.Read();
+        }
+    }
+}
